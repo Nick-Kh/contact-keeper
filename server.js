4c9df0eb1fe4f82b83cbb1ce31ed162e000d8c2e
Nick-Kh <many_sided@hotmail.com>
2020-05-26 01:17:50 +0300

    bug fixes

--- server.js
+++ server.js
@@ -12,6 +12,10 @@ app.use(express.json({ extended: false }));
 
 const PORT = process.env.PORT || 5000;
 
+app.use('/api/users', require('./routes/users'));
+app.use('/api/contacts', require('./routes/contacts'));
+app.use('/api/auth', require('./routes/auth'));
+
 // Serve static assets in production
 if (process.env.NODE_ENV === 'production') {
   // Set static folder
@@ -21,9 +25,5 @@ if (process.env.NODE_ENV === 'production') {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
 }
-
-app.use('/api/users', require('./routes/users'));
-app.use('/api/contacts', require('./routes/contacts'));
-app.use('/api/auth', require('./routes/auth'));
 
 app.listen(PORT, () => console.log(`Server is running on port: ${PORT}`));
