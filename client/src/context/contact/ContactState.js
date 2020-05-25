e5f769ad22f59c8ed0c1136df76966e0baa65ddb
Nick-Kh <many_sided@hotmail.com>
2020-05-26 00:11:52 +0300

    bug fixes

--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -28,10 +28,10 @@ const ContactState = (props) => {
   // Get Contacts
   const getContacts = async () => {
     try {
-      const res = await axios.get('/api/contacts');
+      const res = await axios.get('http://localhost:5000/api/contacts');
       dispatch({ type: GET_CONTACTS, payload: res.data });
     } catch (error) {
-      dispatch({ type: CONTACT_ERROR, payload: 'error.response.msg ' });
+      dispatch({ type: CONTACT_ERROR, payload: error.response.msg });
     }
   };
 
@@ -47,20 +47,24 @@ const ContactState = (props) => {
     };
 
     try {
-      const res = await axios.post('/api/contacts', contact, config);
+      const res = await axios.post(
+        'http://localhost:5000/api/contacts',
+        contact,
+        config
+      );
       dispatch({ type: ADD_CONTACT, payload: res.data });
     } catch (error) {
-      dispatch({ type: CONTACT_ERROR, payload: 'error.response.msg' });
+      dispatch({ type: CONTACT_ERROR, payload: error.response.msg });
     }
   };
 
   // Delete Contact
   const deleteContact = async (id) => {
     try {
-      await axios.delete(`/api/contacts/${id}`);
+      await axios.delete(`http://localhost:5000/api/contacts/${id}`);
       dispatch({ type: DELETE_CONTACT, payload: id });
     } catch (error) {
-      dispatch({ type: CONTACT_ERROR, payload: 'error.response.msg' });
+      dispatch({ type: CONTACT_ERROR, payload: error.response.msg });
     }
   };
 
@@ -83,13 +87,13 @@ const ContactState = (props) => {
     };
     try {
       const res = await axios.put(
-        `/api/contacts/${contact._id}`,
+        `http://localhost:5000/api/contacts/${contact._id}`,
         contact,
         config
       );
       dispatch({ type: UPDATE_CONTACT, payload: res.data });
     } catch (error) {
-      dispatch({ type: CONTACT_ERROR, payload: 'error.response.msg' });
+      dispatch({ type: CONTACT_ERROR, payload: error.response.msg });
     }
   };
 
