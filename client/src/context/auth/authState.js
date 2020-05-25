0acbf38a6209bd2a9d512006556f1a61002aab92
Nick-Kh <many_sided@hotmail.com>
2020-05-26 00:47:06 +0300

    bug fixes

--- client/src/context/auth/authState.js
+++ client/src/context/auth/authState.js
@@ -32,7 +32,7 @@ const AuthState = (props) => {
       setAuthToken(localStorage.token);
     }
     try {
-      const res = await axios.get('http://localhost:5000/api/auth');
+      const res = await axios.get('/api/auth');
 
       dispatch({
         type: USER_LOADED,
@@ -52,11 +52,7 @@ const AuthState = (props) => {
       },
     };
     try {
-      const res = await axios.post(
-        'http://localhost:5000/api/users',
-        formData,
-        config
-      );
+      const res = await axios.post('/api/users', formData, config);
       dispatch({
         type: REGISTER_SUCCESS,
         payload: res.data,
@@ -78,11 +74,7 @@ const AuthState = (props) => {
       },
     };
     try {
-      const res = await axios.post(
-        'http://localhost:5000/api/auth',
-        formData,
-        config
-      );
+      const res = await axios.post('/api/auth', formData, config);
       dispatch({
         type: LOGIN_SUCCESS,
         payload: res.data,
--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -28,7 +28,7 @@ const ContactState = (props) => {
   // Get Contacts
   const getContacts = async () => {
     try {
-      const res = await axios.get('http://localhost:5000/api/contacts');
+      const res = await axios.get('/api/contacts');
       dispatch({ type: GET_CONTACTS, payload: res.data });
     } catch (error) {
       dispatch({ type: CONTACT_ERROR, payload: error.response });
@@ -47,11 +47,7 @@ const ContactState = (props) => {
     };
 
     try {
-      const res = await axios.post(
-        'http://localhost:5000/api/contacts',
-        contact,
-        config
-      );
+      const res = await axios.post('/api/contacts', contact, config);
       dispatch({ type: ADD_CONTACT, payload: res.data });
     } catch (error) {
       dispatch({ type: CONTACT_ERROR, payload: error.response });
@@ -61,7 +57,7 @@ const ContactState = (props) => {
   // Delete Contact
   const deleteContact = async (id) => {
     try {
-      await axios.delete(`http://localhost:5000/api/contacts/${id}`);
+      await axios.delete(`/api/contacts/${id}`);
       dispatch({ type: DELETE_CONTACT, payload: id });
     } catch (error) {
       dispatch({ type: CONTACT_ERROR, payload: error.response });
@@ -87,7 +83,7 @@ const ContactState = (props) => {
     };
     try {
       const res = await axios.put(
-        `http://localhost:5000/api/contacts/${contact._id}`,
+        `/api/contacts/${contact._id}`,
         contact,
         config
       );
